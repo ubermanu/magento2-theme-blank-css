18fcba11f16b96ba7b9dcb81af12e5d4cb47a25c
ubermanu <manu@paca.happy-dev.fr>
2021-05-15 13:27:22 +0200

    minor changes

--- registration.php
+++ registration.php
@@ -2,8 +2,8 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::THEME,'frontend/Ubermanu/blank',__DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Ubermanu/blank', __DIR__);
 
 // Load the theme as a module to have access to csp_whitelist.xml
 // https://magento.stackexchange.com/a/330317
-ComponentRegistrar::register(ComponentRegistrar::MODULE,'Ubermanu_Blank',__DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Ubermanu_Blank', __DIR__);
